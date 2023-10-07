75b1cb34e385bfc46b08fad5f3cfc2df7040c36a
Deepak kumar <59889069+dth99@users.noreply.github.com>
2023-10-07 17:00:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3)   //we are using nodejs, not browser
+
+
+let name = "deepak"
+let age = 18
+let isLoggedIn = true
+
+let state = null;
+
+// number => 2 to power 53
+// bigint 
+// string => "" or ''
+// boolen => true/false
+// null => standalone value
+// undefined => 
+// symbol  => used in react for unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (js engine or language issue)
