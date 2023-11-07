a0ba488f6b4ca7db9afa599c993db0af64bfce9c
Deepak kumar <59889069+dth99@users.noreply.github.com>
2023-11-07 21:45:34 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true     what?
+// The reason is that an equality check == and comarisons> < >= <= work differently
+// Comparisons convert null to a number, treating it as 0.
+// That's why (3) null >= 0 is true and (1) null > 0 is false
+
+
+
+console.log(undefined == 0); // undefined always false
+
+// ===  check strictly even data type
+
+console.log("2" === 2);
+console.log("2" == 2)
