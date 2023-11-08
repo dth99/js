41a8d4dbf0b69ae51b301f336bacb57a80f3a08c
Deepak kumar <59889069+dth99@users.noreply.github.com>
2023-11-08 19:28:59 +0000

    js datatypes

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,30 @@
+// Primitive
+
+// 7 types: String, Number, String, Null, Boolen, undefind Symbol, BigInt
+
+//  Reference (Non primitive) Type return type is all function
+
+// array, Objects, Functions
+
+// symbol type
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id ==anotherId)
+
+console.log(id);
+const bigNumber = 3553353235363633553n
+
+
+const heros = ['shaktiman','naagraj','doga']
+let myObj={
+    name: "deepak",
+    age:22,
+}
+const myFunction =function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction );
+
